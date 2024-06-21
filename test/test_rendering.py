dec25f7cdcd60484824a6a8bcf8334594607ba5e
WU Qi <wilson.over.cloud@gmail.com>
2024-06-21 00:31:26 -0700

    update

--- test/test_rendering.py
+++ test/test_rendering.py
@@ -4,7 +4,9 @@ import numpy as np
 
 import ovrpy
 
-renderername = "optix7"
+# renderername = "optix7"
+renderername = "ospray"
+# renderername = "vulkan"
 # filename = os.path.join(
 #         os.path.abspath(os.path.dirname(__file__)),
 #         "test_scene.json")
@@ -14,8 +16,11 @@ scene = ovrpy.create_scene(filename)
 renderer = ovrpy.create_renderer(renderername)
 
 fbsize = ovrpy.vec2i()
-fbsize.x = 640
-fbsize.y = 480
+# fbsize.x = 640
+# fbsize.y = 480
+
+fbsize.x = 640*4
+fbsize.y = 480*4
 
 renderer.set_fbsize(fbsize)
 
@@ -23,16 +28,16 @@ framebufferdata = ovrpy.FrameBufferData()
 
 renderer.init([], scene, scene.camera)
 renderer.commit()
-renderer.render()
-renderer.swap()
 
-print(scene.camera)
+for i in range(16):
+    renderer.render()
+    renderer.swap()
 
 renderer.mapframe(framebufferdata)
 
 pixeldata = framebufferdata.rgba()
 pixelstats = framebufferdata.stats()
-print("Pixel data:", pixeldata)
+# print("Pixel data:", pixeldata)
 # print("Pixel stats:", pixelstats)
 
 pixeldata = (np.clip(pixeldata, 0, 1) * 255).astype(np.uint8)
@@ -40,6 +45,7 @@ pixeldata = pixeldata.reshape(fbsize.y, fbsize.x, 4)
 
 from PIL import Image  
 im = Image.fromarray(pixeldata)
-im.save("your_file.png")
+im.save("test_rendering.png")
+print("done saving test_rendering.png...")
 
 # LD_PRELOAD=/mnt/scratch/ssd/qadwu/miniconda3/envs/ovrpy/lib/libstdc++.so.6 PYTHONPATH=/mnt/scratch/fast0/qadwu/open-volume-renderer/build/Release python ./test_rendering.py ../data/configs/scene_heatrelease_1atm.json 
